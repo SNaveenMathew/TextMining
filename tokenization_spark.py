194d4892cf80e579617b7fdf79a78bf0d830df4c
Naveen Nathan <naveen.nathan@tredence.com>
2018-04-29 20:21:00 +0530

    Modified: Language detection spark functions working.

--- tokenization_spark.py
+++ tokenization_spark.py
@@ -13,8 +13,11 @@ from nltk.tokenize import sent_tokenize
 
 sent_tokenize_udf = udf(lambda x: sent_tokenize(x), ArrayType(elementType = StringType()))
 
-def tokenize_sentence_nltk_spark(df, in_col):
+def tokenize_sentence_nltk_spark(df, in_col, out_col = None):
     df = df.withColumn(in_col, regexp_replace(str = in_col, pattern = "\n", replacement = ". "))
     df = df.withColumn(in_col, regexp_replace(str = in_col, pattern = "\xa0", replacement = " "))
-    df = df.withColumn(in_col, sent_tokenize_udf(in_col))
+    if out_col is not None:
+        df = df.withColumn(out_col, sent_tokenize_udf(in_col))
+    else:
+        df = df.withColumn(in_col, sent_tokenize_udf(in_col))
     return df
--- util_spark.py
+++ util_spark.py
@@ -1,9 +1,21 @@
 from pyspark.ml.feature import StopWordsRemover
+from util import detect_language
+from pyspark.sql.functions import col, udf
+from pyspark.sql.types import StringType
 
-def remove_stopwords_spark(tokenized, in_column, out_column = None):
+detect_language_udf = udf(lambda x: detect_language(" ".join(x))[0].lang, StringType())
+
+def remove_stopwords_spark(tokenized, in_col, out_col = None):
     if out_column is not None:
         remover = StopWordsRemover(inputCol = in_column, outputCol = out_column)
     else:
         remover = StopWordsRemover(inputCol = in_column)
     stopwords_removed = remover.transform(tokenized)
     return stopwords_removed
+
+def detect_language_spark(df, in_col, out_col = None):
+    if out_col is not None:
+        df = df.withColumn(out_col, detect_language_udf(in_col))
+    else:
+        df = df.withColumn(in_col, detect_language_udf(in_col))
+    return df
