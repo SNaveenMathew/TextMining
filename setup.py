c44737720066756af689596f80cd92e6c598bbfd
Naveen Nathan <naveen.nathan@tredence.com>
2018-05-29 10:37:32 +0530

    Adding setup file and ignoring dist, egg folders

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+# Standard set up file to include the package details
+setup(name='TextMining',
+      version='1.0',
+      description='App for common text mining tasks',
+      author='Naveen Mathew Nathan S.',
+      install_requires = ['treetaggerwrapper', 'pandas', 'langdetect', 'autocorrect', 'nltk',
+      'numpy', 'bs4', 'sklearn', 'fuzzywuzzy', 'pyspark', 'gensim', 'pyLDAvis', 'matplotlib'],
+      author_email='pg13s_nathan@mandevian.com'
+     )
